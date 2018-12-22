1c299225a22baca3114f47a36c19e3303c4ac1cd
Georgy Goliev <v-gegoli@microsoft.com>
2018-12-22 18:10:24 +0200

    Logging to file is added

--- Fails_counter.py
+++ Fails_counter.py
@@ -1,12 +1,37 @@
 import csv
 import glob
+import logging
 import os
 from collections import Counter
+
+
+# ToDo To add help (--help)
+# add filemode="w" to overwrite
+logging.basicConfig(format=# u'%(filename)s'
+                           u'[line:%(lineno)d]# '
+                           u'%(levelname)-8s '
+                           u'[%(asctime)s]  '
+                           u'%(message)s',
+                    filename="sample.log", filemode="w", level=logging.INFO)
 
 # adapted for Python 3.4: without type hinting
 smoke_test_summary_folder = os.path.expanduser(
     r'~\Desktop\RovalSimplifier\SmokeTestAutomation\Output\SmokeTestSummary')  # TODO to check with OneCoreSmokeTest
 fail_test_result_file_name = 'fail_test_result.csv'
+
+
+def count_total_folders(path_to_folder: str) -> list:
+    """ Count all (passed, failed, unfinished) folders in SmokeTest (etc) Summary folder
+    :param path_to_folder: string with the path to Summary folder
+    :return: list of 'Total folders', number
+    """
+
+    try:
+        total = len(os.listdir(path_to_folder))
+        logging.info('Total quantity of folders in %s is %s\n' % (path_to_folder, total))
+        return ['Total folders', total]
+    except StopIteration:
+        return ['Total folders', 'empty']
 
 
 def count_fail_tests(path_to_checked_folder: str, file_name_mask: str) -> dict:
@@ -18,30 +43,19 @@ def count_fail_tests(path_to_checked_folder: str, file_name_mask: str) -> dict:
     """
 
     tests_failed = []  # list of all failed tests
-    for file_by_mask in glob.glob(path_to_checked_folder+r'\*Fail*'+file_name_mask):
+    for file_by_mask in glob.glob(path_to_checked_folder + r'\*Fail*' + file_name_mask):
         with open(file_by_mask) as f:
             reader = csv.reader(f)  # reads the file as comma separated values in line
+            logging.info("%s file is checked.\n" % file_by_mask)
             try:
                 for line in reader:  # the cycle is looking for line with test status failed
                     if 'Fail' in line:
-                        tests_failed.append(line[0])  # if exists - adds number (only) of test to the "tests_failed" list
+                        tests_failed.append(
+                            line[0])  # if exists - adds number (only) of test to the "tests_failed" list
             except csv.Error:  # If the file is created not correctly, there is an error 'line contains NULL byte'
-                print('One of files contains NULL byte and isn\'t counted')  # Todo May be to add it to report
+                logging.error('This files contains NULL byte and isn\'t counted.\n')  # Todo May be to add it to report
     dict_with_fails = dict(Counter(tests_failed))
     return dict_with_fails
-
-
-def count_total_folders(path_to_folder: str) -> list:
-    """ Count all (passed, failed, unfinished) folders in SmokeTest (etc) Summary folder    
-    :param path_to_folder: string with the path to Summary folder
-    :return: list of 'Total folders', number
-    """
-
-    try:
-        total = len(os.listdir(path_to_folder))
-        return ['Total folders', total]
-    except StopIteration:
-        return ['Total folders', 'empty']
 
 
 def write_result_file(file_name: str, total_folders: list, failed_test: dict) -> None:
@@ -60,14 +74,13 @@ def write_result_file(file_name: str, total_folders: list, failed_test: dict) ->
             file_writer.writerow(test_number_and_fails)
 
 
-fail_test_result: dict = count_fail_tests(smoke_test_summary_folder, r'\Smoke*')
-# Intermediate variable with result of function. Just to do help the understanding and visualisation
+logging.info('Program started')
 
 total_folders_result = count_total_folders(smoke_test_summary_folder)
+# Intermediate variable with result of function. Just to do help the understanding and visualisation
+fail_test_result: dict = count_fail_tests(smoke_test_summary_folder, r'\Smoke*')
 write_result_file(fail_test_result_file_name, total_folders_result, fail_test_result)
-
 
 print('Print for debugging only! Done.')
 print(total_folders_result)
 print(fail_test_result)
-
